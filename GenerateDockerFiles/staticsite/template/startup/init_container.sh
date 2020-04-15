525878617f9f30ffb79209e2ef6a976d3519232b
Purva Vasudeo <puvasude@microsoft.com>
2020-04-15 11:02:17 -0700

    Removed check for PORT in init_container.sh

--- GenerateDockerFiles/staticsite/template/startup/init_container.sh
+++ GenerateDockerFiles/staticsite/template/startup/init_container.sh
@@ -7,18 +7,10 @@ eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 /usr/sbin/sshd
 
-# check if $PORT exists, else set it to 8080
-if [[ -z "${PORT}" ]]; then
-    PORT="8080"
-    export PORT="$PORT"
-else
-    PORT="${PORT}"
-fi
-
 # replace occurence of PORT in config site file for nginx
 sed -i "s/PORT/$PORT/g" /etc/nginx/sites-available/mysite
 ln -s /etc/nginx/sites-available/mysite /etc/nginx/sites-enabled/mysite
-# nginx -g 'daemon off;'
+
 echo "Restarting nginx..."
 service nginx restart
 
