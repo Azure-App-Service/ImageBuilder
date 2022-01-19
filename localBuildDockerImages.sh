30828a964f2cec6acb227f41b31a335367c055f5
Amit Kumar <kumaramit@microsoft.com>
2022-01-19 09:02:19 +0530

    Build DiagServer Image using ImageBuilder

--- localBuildDockerImages.sh
+++ localBuildDockerImages.sh
@@ -35,3 +35,6 @@ BuildAndTagImages/build.sh $DockerFileDir appsvctest "Config" $BuildNumber $Stac
 # dynamic install
 StackName="KuduLite"
 BuildAndTagImages/build.sh $DockerFileDir appsvctest "Config" $BuildNumber $StackName "PullRequest" "GitRepo-DynInst" dynamic-$BuildNumber
+
+StackName="DiagnosticServer"
+BuildAndTagImages/build.sh $DockerFileDir appsvctest "Config" $BuildNumber $StackName "PullRequest" "GitRepo" $BuildNumber
