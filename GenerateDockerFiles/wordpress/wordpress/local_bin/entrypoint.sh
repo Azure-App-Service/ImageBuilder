75a57b5e7e620a03b82dfe85a58fbdf0a0a66af0
saisubodh_MSFT <saisubodh@microsoft.com>
2022-03-21 18:11:27 +0530

    WordPress: Display service page until WordPress is installed

--- GenerateDockerFiles/wordpress/wordpress/local_bin/entrypoint.sh
+++ GenerateDockerFiles/wordpress/wordpress/local_bin/entrypoint.sh
@@ -46,6 +46,19 @@ update_php_config() {
 	fi
 }
 
+dummy_server_start() {
+    mkdir -p /home/site/temp-root
+    cp $WORDPRESS_SOURCE/hostingstart.html /home/site/temp-root/hostingstart.html
+    cp /usr/src/nginx/dummy-server.conf /etc/nginx/conf.d/default.conf
+    /usr/sbin/nginx
+}
+
+dummy_server_stop() {
+    #kill any existing nginx processes
+    killall nginx 2> /dev/null 
+    rm -rf /home/site/temp-root
+}
+
 setup_wordpress() {
     if [ ! -d $WORDPRESS_LOCK_HOME ]; then
         mkdir -p $WORDPRESS_LOCK_HOME
@@ -58,42 +71,36 @@ setup_wordpress() {
         echo "INFO: Found an existing WordPress status file ..."
     fi
 
-    if [ ! $(grep "GIT_PULL_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
-        local IS_GIT_PULL_SUCCESS="FALSE"
+    if [[ ! $BLOB_STORAGE_ENABLED && $(grep "W3TC_PLUGIN_CONFIG_UPDATED" $WORDPRESS_LOCK_FILE) ]] ||  [[ $BLOB_STORAGE_ENABLED &&  $(grep "BLOB_STORAGE_CONFIG_UPDATED" $WORDPRESS_LOCK_FILE) ]]; then
+        if [ ! $(grep "FIRST_TIME_SETUP_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
+            echo "FIRST_TIME_SETUP_COMPLETED" >> $WORDPRESS_LOCK_FILE
+        fi
+    fi
+    
+    #Start server with static webpage until wordpress is installed
+    if [ ! $(grep "FIRST_TIME_SETUP_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
+        dummy_server_start
+    fi
+    
+    if [ $(grep "GIT_PULL_COMPLETED" $WORDPRESS_LOCK_FILE) ] &&  [ ! $(grep "WORDPRESS_PULL_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
+        echo "WORDPRESS_PULL_COMPLETED" >> $WORDPRESS_LOCK_FILE
+    fi
+    
+    if [ ! $(grep "WORDPRESS_PULL_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
         while [ -d $WORDPRESS_HOME ]
         do
             mkdir -p /home/bak
             mv $WORDPRESS_HOME /home/bak/wordpress_bak$(date +%s)            
         done
         
-        GIT_REPO=${GIT_REPO:-https://github.com/azureappserviceoss/wordpress-azure}
-	    GIT_BRANCH=${GIT_BRANCH:-linux-appservice}
-	    echo "INFO: ++++++++++++++++++++++++++++++++++++++++++++++++++:"
-	    echo "REPO: "$GIT_REPO
-	    echo "BRANCH: "$GIT_BRANCH
-	    echo "INFO: ++++++++++++++++++++++++++++++++++++++++++++++++++:"
-
-        if git clone $GIT_REPO $WORDPRESS_HOME && cd $WORDPRESS_HOME; then
-            if [ "$GIT_BRANCH" != "master" ]; then
-                if git fetch origin \
-                && git branch --track $GIT_BRANCH origin/$GIT_BRANCH \
-                && git checkout $GIT_BRANCH; then
-                    IS_GIT_PULL_SUCCESS="TRUE"
-                fi
-            else
-                IS_GIT_PULL_SUCCESS="TRUE"
-            fi
-        fi
-
-        #remove .git
-        rm  -rf $WORDPRESS_HOME/.git
-
-        if [ "$IS_GIT_PULL_SUCCESS" == "TRUE" ]; then
-            echo "GIT_PULL_COMPLETED" >> $WORDPRESS_LOCK_FILE
+        mkdir -p $WORDPRESS_HOME
+        echo "INFO: Pulling WordPress code"
+        if cp -r $WORDPRESS_SOURCE/wordpress-azure/* $WORDPRESS_HOME; then
+            echo "WORDPRESS_PULL_COMPLETED" >> $WORDPRESS_LOCK_FILE
         fi
     fi
 
-    if [ $(grep "GIT_PULL_COMPLETED" $WORDPRESS_LOCK_FILE) ] &&  [ ! $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
+    if [ $(grep "WORDPRESS_PULL_COMPLETED" $WORDPRESS_LOCK_FILE) ] &&  [ ! $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ]; then
         if wp core install --url=$WEBSITE_HOSTNAME --title="${WORDPRESS_TITLE}" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --path=$WORDPRESS_HOME --allow-root; then
             echo "WP_INSTALLATION_COMPLETED" >> $WORDPRESS_LOCK_FILE
         fi
@@ -112,8 +119,7 @@ setup_wordpress() {
     fi
 
     if [ $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ] && [ ! $(grep "SMUSH_PLUGIN_INSTALLED" $WORDPRESS_LOCK_FILE) ]; then
-        wp plugin is-installed wp-smushit --path=$WORDPRESS_HOME --allow-root
-        if [ $? -eq 0 ]; then
+        if wp plugin is-installed wp-smushit --path=$WORDPRESS_HOME --allow-root; then
             echo "SMUSH_PLUGIN_INSTALLED" >> $WORDPRESS_LOCK_FILE
         elif wp plugin install wp-smushit --force --activate --path=$WORDPRESS_HOME --allow-root; then
             echo "SMUSH_PLUGIN_INSTALLED" >> $WORDPRESS_LOCK_FILE
@@ -133,8 +139,7 @@ setup_wordpress() {
     fi
 
     if [ $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ] && [ ! $(grep "W3TC_PLUGIN_INSTALLED" $WORDPRESS_LOCK_FILE) ]; then
-        wp plugin is-installed w3-total-cache --path=$WORDPRESS_HOME --allow-root
-        if [ $? -eq 0 ]; then
+        if wp plugin is-installed w3-total-cache --path=$WORDPRESS_HOME --allow-root; then
             echo "W3TC_PLUGIN_INSTALLED" >> $WORDPRESS_LOCK_FILE
         elif wp plugin install w3-total-cache --force --activate --path=$WORDPRESS_HOME --allow-root; then
             echo "W3TC_PLUGIN_INSTALLED" >> $WORDPRESS_LOCK_FILE
@@ -149,9 +154,8 @@ setup_wordpress() {
         fi
     fi
     
-    if [ $BLOB_STORAGE_ENABLED ] && [ $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ] && [ ! $(grep "BLOB_STORAGE_PLUGIN_INSTALLED" $WORDPRESS_LOCK_FILE) ]; then  
-        wp plugin is-installed windows-azure-storage --path=$WORDPRESS_HOME --allow-root
-        if [ $? -eq 0 ]; then
+    if [ $BLOB_STORAGE_ENABLED ] && [ $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ] && [ ! $(grep "BLOB_STORAGE_PLUGIN_INSTALLED" $WORDPRESS_LOCK_FILE) ]; then
+        if wp plugin is-installed windows-azure-storage --path=$WORDPRESS_HOME --allow-root; then
             echo "BLOB_STORAGE_PLUGIN_INSTALLED" >> $WORDPRESS_LOCK_FILE
         elif wp plugin install windows-azure-storage --force --activate --path=$WORDPRESS_HOME --allow-root; then
             echo "BLOB_STORAGE_PLUGIN_INSTALLED" >> $WORDPRESS_LOCK_FILE
@@ -182,6 +186,10 @@ setup_wordpress() {
 }
 
 echo "Setup openrc ..." && openrc && touch /run/openrc/softlevel
+
+test ! -d "$SUPERVISOR_LOG_DIR" && echo "INFO: $SUPERVISOR_LOG_DIR not found. creating ..." && mkdir -p "$SUPERVISOR_LOG_DIR"
+test ! -d "$NGINX_LOG_DIR" && echo "INFO: Log folder for nginx/php not found. creating..." && mkdir -p "$NGINX_LOG_DIR"
+test ! -e /home/50x.html && echo "INFO: 50x file not found. creating..." && cp /usr/share/nginx/html/50x.html /home/50x.html
 
 if ! [[ $SKIP_WP_INSTALLATION ]] || ! [[ "$SKIP_WP_INSTALLATION" == "true" 
     || "$SKIP_WP_INSTALLATION" == "TRUE" || "$SKIP_WP_INSTALLATION" == "True" ]]; then
@@ -249,10 +257,6 @@ if [ ! $AZURE_DETECTED ]; then
     crond	
 fi 
 
-test ! -d "$SUPERVISOR_LOG_DIR" && echo "INFO: $SUPERVISOR_LOG_DIR not found. creating ..." && mkdir -p "$SUPERVISOR_LOG_DIR"
-test ! -d "$NGINX_LOG_DIR" && echo "INFO: Log folder for nginx/php not found. creating..." && mkdir -p "$NGINX_LOG_DIR"
-test ! -e /home/50x.html && echo "INFO: 50x file not found. creating..." && cp /usr/share/nginx/html/50x.html /home/50x.html
-
 
 #Updating php configuration values
 if [[ -e $PHP_CUSTOM_CONF_FILE ]]; then
@@ -273,10 +277,17 @@ touch /run/php/php-fpm.sock
 chown nginx:nginx /run/php/php-fpm.sock
 chmod 777 /run/php/php-fpm.sock
 
+
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 echo "Starting SSH ..."
 echo "Starting php-fpm ..."
 echo "Starting Nginx ..."
+
+#stop dummy server
+dummy_server_stop
+
+#ensure correct default.conf before starting/reloading WordPress server
+cp /usr/src/nginx/wordpress-server.conf /etc/nginx/conf.d/default.conf
 
 cd /usr/bin/
 supervisord -c /etc/supervisord.conf
